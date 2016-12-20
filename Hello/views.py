d3ac74b6ee5000cfa8a16ab851b157d5a5acdefa
Seamus Woods <seamuswoods3@gmail.com>
2016-12-20 02:21:17 +0000

    urls

--- Hello/views.py
+++ Hello/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import render_to_response
+# from django.template import RequestContext
 
+def index(request):
+    return render_to_response('Home.html')
 
 # Create your views here.
-def get_home(request):
-    return render(request, 'Home.html')
+# def get_home(request):
+#     return render(request, 'Home.html')
 
 def get_index_1(request):
     return render(request, 'About.html')
@@ -20,5 +24,5 @@ def get_Contact(request):
 def get_Privacy(request):
     return render(request, 'Privacy-Policy.html')
 
-def get_Products(request):
-    return render(request, 'products.html')
+# def get_Products(request):
+#     return render(request, 'products.html')
