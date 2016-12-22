090566486237b6189937d0102cebfc8f27633754
Seamus Woods <seamuswoods3@gmail.com>
2016-12-22 00:39:26 +0000

    some images

--- blog/views.py
+++ blog/views.py
@@ -75,14 +75,14 @@ from .models import Post
 from .forms import BlogPostForm
 
 
-def blog_posts(request):
-    posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, "blogposts.html", {'posts':posts})
+# def blog_posts(request):
+#     posts= Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+#     return render(request, "blogposts.html", {'posts':posts})
 
-# def post_list(request):
-#     posts = Post.objects.filter(published_date__lte=timezone.now()
-#                                 ).order_by('-published_date')
-#     return render(request, "blogposts.html", {'posts': posts})
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
 
 
 def post_detail(request, id):
