ceb5ac0105e7b6e15b11e3c4eb15a7a0735212d7
Seamus Woods <seamuswoods3@gmail.com>
2016-12-22 00:01:45 +0000

    blog

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,102 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from django.utils import timezone
+# from .models import Post
+# from django.shortcuts import render, get_object_or_404
+# from .forms import BlogPostForm
+# from django.shortcuts import redirect
+#
+#
+# # Create your views here.
+#
+# def post_list(request):
+#
+#     """
+#     Create a view that will return a list
+#     of Posts that were published prior to 'now'
+#     and render them to 'blogposts.html' template
+#     """
+#
+#
+#
+#
+#
+#     posts = Post.objects.all()
+#     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+#     return render(request, "blogposts.html", {'posts': posts})
+#
+# def post_detail(request, id):
+#     """
+#     Create a view that return a single
+#     Post object based on the post ID and
+#     and render it to the 'postdetail.html'
+#     template. Or return a 404 error if the
+#     post is not found
+#     """
+#     post = get_object_or_404(Post, pk=id)
+#     post.views += 1  # clock up the number of post views
+#     post.save()
+#     return render(request, "postdetail.html", {'post': post})
+#
+#
+#
+# def new_post(request):
+#     if request.method == "POST":
+#         form = BlogPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect(post_detail, post.pk)
+#     else:
+#         form = BlogPostForm()
+#     return render(request, 'blogpostform.html', {'form': form})
+#
+#
+# def edit_post(request, id):
+#    post = get_object_or_404(Post, pk=id)
+#    if request.method == "POST":
+#        form = BlogPostForm(request.POST, request.FILES, instance=post)
+#        if form.is_valid():
+#            post = form.save(commit=False)
+#            post.author = request.user
+#            post.published_date = timezone.now()
+#            post.save()
+#            return redirect(post_detail, post.pk)
+#    else:
+#        form = BlogPostForm(instance=post)
+#    return render(request, 'blogpostform.html', {'form': form})
+#
+#
+
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 from .forms import BlogPostForm
-from django.shortcuts import redirect
-
-
-# Create your views here.
 
 def post_list(request):
-
-    """
-    Create a view that will return a list
-    of Posts that were published prior to 'now'
-    and render them to 'blogposts.html' template
-    """
-    posts = Post.objects.all()
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()
+                                ).order_by('-published_date')
     return render(request, "blogposts.html", {'posts': posts})
 
+
 def post_detail(request, id):
-    """
-    Create a view that return a single
-    Post object based on the post ID and
-    and render it to the 'postdetail.html'
-    template. Or return a 404 error if the
-    post is not found
-    """
     post = get_object_or_404(Post, pk=id)
-    post.views += 1  # clock up the number of post views
+    post.views += 1
     post.save()
     return render(request, "postdetail.html", {'post': post})
 
 
+def home(request):
+    return render(request, "home.html")
+
+
+def login(request):
+    return render(request, "login.html")
+
+
+def register(request):
+    return render(request, "register.html")
 
 def new_post(request):
     if request.method == "POST":
@@ -47,7 +111,6 @@ def new_post(request):
         form = BlogPostForm()
     return render(request, 'blogpostform.html', {'form': form})
 
-
 def edit_post(request, id):
    post = get_object_or_404(Post, pk=id)
    if request.method == "POST":
@@ -61,6 +124,3 @@ def edit_post(request, id):
    else:
        form = BlogPostForm(instance=post)
    return render(request, 'blogpostform.html', {'form': form})
-
-
-
