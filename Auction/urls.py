cf6c9f792f34d5db8cf32a8fc021f53866f75802
Seamus Woods <seamuswoods3@gmail.com>
2016-12-23 06:12:30 +0000

    tidy up

--- Auction/urls.py
+++ Auction/urls.py
@@ -22,31 +22,17 @@ from Hello import views
 from blog import urls as blog_urls
 from paypal_store import views as paypal_views
 from paypal.standard.ipn import urls as paypal_urls
-# from sell import views as sell_views
-#from accounts import urls
-# from gallery import urls as gallery_urls
-
-
 
 urlpatterns = [
-    # url(r'^$', 'Hello.views.index'),
-
-
-
-
+    
      url(r'^$', views.get_home, name='home'),
     url(r'^home/$', views.get_home, name='home'),
-    # url(r'^about/$', views.get_index_1, name='about'),
-    # url(r'^about/', include('gallery.urls')),
-    #url(r'^sell/$', views.get_Sell, name='sell'),
     url(r'^buy/$', views.get_Buy, name='buy'),
     url(r'^blog/', include(blog_urls)),
     url(r'^contact/$',views.get_Contact, name='contact'),
     url(r'^privacy/$',views.get_Privacy, name='privacy'),
     url(r'^gallery/', views.get_index_1, name='gallery'),
-    # url(r'^product/$',views.get_Products, name= 'products' ),
     url(r'^admin/', admin.site.urls),
-    #url(r'', include('blog.urls')),
     url(r'^static/(?P<path>.*)$', static_views.serve),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'', include('accounts.urls')),
@@ -54,8 +40,5 @@ urlpatterns = [
     url(r'^a-very-hard-to-guess-url/', include(paypal_urls)),
     url(r'^paypal-return/', paypal_views.paypal_return),
     url(r'^paypal-cancel/', paypal_views.paypal_cancel),
-    # url(r'^gallery/$', include_urls)
-    # url(r'', include('gallery.urls')),
-
 
 ]
